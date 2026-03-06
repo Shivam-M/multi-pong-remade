36fbfd24ee8e56017c64ff65eaa7bb941a68c2e2
Shivam-M <shivammann0@gmail.com>
2026-03-06 16:39:37 +0000

    java: add a functional but headless client

---
+++ java/src/main/java/com/sm/Client.java
@@ -0,0 +1,158 @@
+package com.sm;
+
+import com.google.protobuf.InvalidProtocolBufferException;
+import com.sm.protobufs.Pong.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.*;
+import java.net.*;
+import java.nio.ByteBuffer;
+
+import static com.sm.Constants.*;
+
+public class Client {
+
+    private static final Logger logger = LoggerFactory.getLogger(Client.class);
+
+    private final DatagramSocket serverSocket;
+    private final InetSocketAddress coordinatorAddress;
+    private InetSocketAddress serverAddress;
+    private Socket coordinatorSocket;
+    private DataOutputStream coordinatorOutputStream;
+    private DataInputStream coordinatorInputStream;
+
+    public volatile State state;
+    private String token;
+
+    public Client(InetSocketAddress coordinator) {
+        try {
+            serverSocket = new DatagramSocket();
+        } catch (IOException e) {
+            logger.error("Failed to create a server socket", e);
+            throw new RuntimeException(e);
+        }
+
+        Player.Builder player = Player.newBuilder()
+                .setPaddleLocation(0.5f)
+                .setPaddleDirection(Direction.STOP)
+                .setScore(0);
+
+        state = State.newBuilder()
+                .setBall(Ball.newBuilder().setX(0.5f).setY(0.5f).build())
+                .setPlayer1(player.setIdentifier(Player.Identifier.PLAYER_1).build())
+                .setPlayer2(player.setIdentifier(Player.Identifier.PLAYER_2).build())
+                .setFrame(0)
+                .setToken("")
+                .build();
+
+        coordinatorAddress = coordinator;
+
+        try {
+            connectCoordinator();
+        } catch (IOException e) {
+            logger.error("Failed to connect to the game coordinator at {}", coordinatorAddress);
+            logger.trace("Exception connecting to the game coordinator: ", e);
+            return;
+        }
+
+        Thread listenCoordinatorThread = new Thread(this::listenCoordinator);
+        listenCoordinatorThread.start();
+
+        sendMessageToCoordinator(Message.newBuilder().setSearch(Search.getDefaultInstance()).build());
+
+        try {
+            listenCoordinatorThread.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    void connectCoordinator() throws IOException {
+        coordinatorSocket = new Socket(coordinatorAddress.getAddress(), coordinatorAddress.getPort());
+        coordinatorOutputStream = new DataOutputStream(coordinatorSocket.getOutputStream());
+        coordinatorInputStream = new DataInputStream(coordinatorSocket.getInputStream());
+    }
+
+    void listenCoordinator() {
+        try {
+            while (!Thread.currentThread().isInterrupted()) {
+                // should use parse/writeDelimitedTo for TCP, but it requires change across all languages for parity
+                byte[] buffer = new byte[MULTI_PONG_SERVER_BUFFER];
+                int read = coordinatorInputStream.read(buffer);
+
+                if (read == -1) break;
+
+                Message message = Message.parseFrom(ByteBuffer.wrap(buffer,0, read));
+
+                if (message.hasMatch()) {
+                    handleMatch(message.getMatch());
+                } else {
+                    logger.warn("Received an unexpected message from the coordinator: {}", message);
+                }
+
+            }
+        } catch (IOException e) {
+            logger.trace("Exception listening to the game coordinator", e);
+        }
+        logger.error("Lost connection to the game coordinator");
+    }
+
+    void sendMessageToCoordinator(Message message) {
+        try {
+            logger.debug("Sending {} to the coordinator...", message);
+            message.writeTo(coordinatorOutputStream);
+            coordinatorOutputStream.flush();
+        } catch (IOException e) {
+            logger.error("Failed to send message", e);
+        }
+    }
+
+    void handleMatch(Match match) {
+        token = match.getToken();
+        serverAddress = new InetSocketAddress(match.getHost(), match.getPort());
+
+        new Thread(this::listenServer).start();
+
+        sendMessageToServer(Message.newBuilder().setJoin(Join.newBuilder().setToken(token).build()).build());
+    }
+
+    void listenServer() {
+        DatagramPacket receivePacket = new DatagramPacket(new byte[MULTI_PONG_SERVER_BUFFER], MULTI_PONG_SERVER_BUFFER);
+        InetSocketAddress packetAddress;
+        while (!Thread.currentThread().isInterrupted()) {
+            try {
+                serverSocket.receive(receivePacket);
+                packetAddress = new InetSocketAddress(receivePacket.getAddress(), receivePacket.getPort());
+                Message message = Message.parseFrom(ByteBuffer.wrap(receivePacket.getData(),0, receivePacket.getLength()));
+
+                if (message.hasState()) {
+                    if (message.getState().getToken().equals(token)) {
+                        state = message.getState();
+                    } else {
+                        logger.warn("Received a game state message with an invalid token");
+                    }
+                } else {
+                    logger.warn("Received an unexpected or malformed message from {}", packetAddress);
+                }
+            } catch (InvalidProtocolBufferException e) {
+                logger.warn("Received malformed message while listening for messages from {}", serverAddress, e);
+            } catch (IOException e) {
+                logger.trace("IO error while listening for packets from {}", serverAddress, e);
+            }
+        }
+    }
+
+    void sendMessageToServer(Message message) {
+        byte[] messageBytes = message.toByteArray();
+        DatagramPacket messagePacket = new DatagramPacket(messageBytes, messageBytes.length);
+        messagePacket.setAddress(serverAddress.getAddress());
+        messagePacket.setPort(serverAddress.getPort());
+
+        try {
+            serverSocket.send(messagePacket);
+        } catch (IOException e) {
+            logger.error("Error sending message {} to {}", message, serverAddress);
+        }
+    }
+}
--- java/src/main/java/com/sm/Main.java
+++ java/src/main/java/com/sm/Main.java
@@ -3,13 +3,16 @@ package com.sm;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.net.InetSocketAddress;
+
 public class Main {
 
     private static final Logger logger = LoggerFactory.getLogger(Main.class);
 
     public static void main(String[] args) {
         // Coordinator coordinator = new Coordinator();
-        Server server = new Server();
+        // Server server = new Server();
+        Client client = new Client(new InetSocketAddress("127.0.0.1", 4999));
     }
 
 }
